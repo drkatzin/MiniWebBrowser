4ce42e2b91449624ab7a7d736e9c4a3e4cac40bb
Dustin Katzin <drkatzin@Dustins-MacBook-Pro.local>
2017-09-06 23:56:32 -0400

    Completed storyboard layout, outlets and actions

--- MiniWebBrowser/ViewController.swift
+++ MiniWebBrowser/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Outlets
+    
+    @IBOutlet weak var urlInput: UITextField!
+    
+    //MARK: Actions
+    
+    @IBAction func goToURL(_ sender: Any) {
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+    }
+    
+    @IBAction func goForward(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
