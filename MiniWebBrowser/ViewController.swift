33067bd8b0187cec7b89801651f458744a2c93c9
Dustin Katzin <drkatzin@gmail.com>
2017-09-07 00:10:39 -0400

    Implemented preliminary ViewController logic

--- MiniWebBrowser/ViewController.swift
+++ MiniWebBrowser/ViewController.swift
@@ -10,24 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var browserModel : BrowserModel
+    
     //MARK: Outlets
     
     @IBOutlet weak var urlInput: UITextField!
+    @IBOutlet weak var webViewDisplay: UIWebView!
     
     //MARK: Actions
     
+    func displayURLContents(urlString: String) {
+        let url = URL(string: urlString)!
+        webViewDisplay.loadRequest(URLRequest(url: url))
+        //TO DO: resolve race condition
+        //Should finish loading before allowing the user to make another loadRequest
+    }
+    
     @IBAction func goToURL(_ sender: Any) {
+        
+        if let newURL = urlInput.text {
+            browserModel.visitNewURL(url: newURL)
+            displayURLContents(urlString: browserModel.currentURL)
+        }
     }
     
     @IBAction func goBack(_ sender: Any) {
+        browserModel.goBack()
+        displayURLContents(urlString: browserModel.currentURL)
     }
     
     @IBAction func goForward(_ sender: Any) {
+        browserModel.goForward()
+        displayURLContents(urlString: browserModel.currentURL)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        browserModel = BrowserModel()
     }
 
     override func didReceiveMemoryWarning() {
